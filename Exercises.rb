72e231d4b45f1c6852c8f24c8c9e1de3a6bc434c
Denise <yu.denise.d@gmail.com>
2014-09-15 14:27:08 +0100

    Add Exercises.rb

---
+++ Exercises.rb
@@ -0,0 +1,7 @@
+# Write a unit test for the map() method, then reopen the Array class and implement it.
+
+# map { |item| block } --> new_ary
+
+Movies = [ "Castaway", "Forrest Gump", "Saving Private Ryan", "Charlie Wilson's War" ]
+
+puts Movies.map = { |x| x + "!" }
